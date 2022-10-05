c096d560e326a4c50d11b9af0ab86d40491ae361
Muzamil-Hashmi <MUZAMIL.RELYMERLABS@GMAIL.COM>
2022-10-05 13:17:25 +0500

    tem-folders

---
+++ src/component/element/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+      <h1 className='display-1 fw-bold text-primary mt-5  text-center' >Hero</h1>
+    </div>
+  )
+}
