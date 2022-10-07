ceba3583e671c9a62623570536dba03142a39d67
Muzamil-Hashmi <MUZAMIL.RELYMERLABS@GMAIL.COM>
2022-10-07 13:00:29 +0500

    Final

--- src/component/element/Documents.jsx
+++ src/component/element/Documents.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import {BsFillCheckCircleFill} from 'react-icons/bs';
+import React from "react";
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
-export default function 
-() {
+export default function () {
   return (
- 
-        <div className="container-fluid" id='Doc'>
-      <div className="container my-5" id='Con'>
-
-
+    <div className="container-fluid" id="Doc">
+      <div className="container my-5 " id="Con">
         <div className="row">
+          <div className="col-md-8 mt-5">
+            <h3>White Paper</h3>
+            <h1>Read Our Documents</h1>
 
-
-            <div className="col-md-8">
-<h3>White Paper</h3>
-<h1>
-    Read Our Documents
-</h1>
-<div className='d-flex flex-wrap my-5'>
+            <p>
+              {" "}
+              <BsFillCheckCircleFill size="30px" color="orange" /> KYC
+              <BsFillCheckCircleFill
+                size="30px"
+                color="orange"
+                className="ms-4"
+              />{" "}
+              Audit
+              <BsFillCheckCircleFill
+                size="30px"
+                color="orange"
+                className="ms-4"
+              />{" "}
+              Whitepaper
+            </p>
+            {/* <div className='d-flex flex-wrap my-5'>
 
 <div className='d-flex '>
 <BsFillCheckCircleFill size="30px" color='orange'/> 
@@ -31,20 +40,35 @@ export default function
 <BsFillCheckCircleFill size="30px" color='orange' /> 
 <h1>Paper</h1>
 </div> 
-</div>
+</div> */}
 
-<div className="dropdown">
-  <button className="dropbtn" type="button" data-bs-toggle="dropdown" >
-    Download
-  </button>
-  <ul className="dropdown-content dropdown-menu-white ">
-    <li><a className="dropdown-item active" href="#">KYC</a></li>
-    <li><a className="dropdown-item" href="#">Audit</a></li>
-    <li><a className="dropdown-item" href="#">Paper</a></li>
-    
-  </ul>
-</div>
-{/* <div class="dropdown">
+            <div className="dropdown">
+              <button
+                className="dropbtn"
+                type="button"
+                data-bs-toggle="dropdown"
+              >
+                Download
+              </button>
+              <ul className="dropdown-content dropdown-menu-white ">
+                <li>
+                  <a className="dropdown-item active" href="#">
+                    KYC
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Audit
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Paper
+                  </a>
+                </li>
+              </ul>
+            </div>
+            {/* <div class="dropdown">
   <button class="dropbtn">Dropdown</button>
   <div class="dropdown-content">
   <a href="#">Link 1</a>
@@ -52,17 +76,14 @@ export default function
   <a href="#">Link 3</a>
   </div>
 </div> */}
-
-
-            </div>
-            <div className="col-md-4">
-
-            <a className="navbar-brand" href="#"><img src="assets/bolll.png" alt=" "width={270} /></a>
-
-            </div>
+          </div>
+          <div className="col-md-4">
+            <a className="navbar-brand" href="#">
+              <img src="assets/bolll.png" alt=" " width={270} />
+            </a>
+          </div>
         </div>
       </div>
-      </div>
-   
-  )
+    </div>
+  );
 }
